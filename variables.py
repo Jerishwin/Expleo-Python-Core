6c23e866dc81fd068753d3da7111e60339e3fb78
Jerishwin Joseph <jerishwinj@gmail.com>
2026-05-21 16:49:14 +0530

    21-05-2026

--- variables.py
+++ variables.py
@@ -7,3 +7,56 @@ print(type(var))
 
 name = "Jolly"
 print(type(name))
+
+list =[5,3.4,"srt hell","20",45]
+
+print(list)
+print(type(list))
+
+list1 =(5,3.4,"srt hell","20",45)
+
+print(list1)
+print(type(list1))
+
+list2 ={5,3.4,"srt hell","20",45}
+
+print(list2)
+print(type(list2))
+
+var1 = None
+print(type(var1))
+
+list3 ={"Name":"Jeri","age":21}
+
+print(list3)
+print(type(list3))
+print(list3["age"])
+
+x = (1==True)
+y= (1==False)
+a= True+4
+b= False+10
+
+print("x is",x)
+print("y is",y)
+print("a is",a)
+print("b is",b)
+
+num1 = 5
+num2=num1
+num3 =5
+print(num1 is num2)
+print(num1 is num3)
+
+print(num1 is not num2)
+print(num1 is not num3)
+
+print(5 in list2)
+print(5 not in list1)
+
+sum1 = num1+num2
+print(sum1)
+print(type(sum1))
+sum1 = float(sum1)
+print(sum1)
+print(type(sum1))
